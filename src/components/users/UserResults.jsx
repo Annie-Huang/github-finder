0ebd9ac912f3c6252fad570c9abaa6fd62aca9e8
Annie Huang <annie.hn@gmail.com>
2022-03-18 17:52:34 +1100

    The github token is too troublesome, decide to remove it and fall back to max 10 calls per hour

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -10,11 +10,12 @@ const UserResults = () => {
   }, []);
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
+    // const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+    //   headers: {
+    //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    //   },
+    // });
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`);
 
     const data = await response.json();
     console.log(data);
