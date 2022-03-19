79d5fdfb732d02ded088ed46ae4acefc2ffe7b95
Annie Huang <annie.hn@gmail.com>
2022-03-19 15:05:44 +1100

    Create the basic UserItem component and loop through users in UserResults to call it.

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+
+const UserItem = ({ user: { login, avatar_url } }) => {
+  return <div>{login}</div>;
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+export default UserItem;
