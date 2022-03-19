6aa3dee073b190ebd1997d0a322072bf034c3ff3
Annie Huang <annie.hn@gmail.com>
2022-03-19 18:46:29 +1100

    Add 'SET_LOADING' into reducer. Changed intial state and change setLoading before fetch call is made.

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -26,12 +26,14 @@ export const GithubProvider = ({ children }) => {
 
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
+    setLoading();
+
     // const response = await fetch(`${GITHUB_URL}/users`, {
     //   headers: {
     //     Authorization: `token ${GITHUB_TOKEN}`,
@@ -42,6 +44,9 @@ export const GithubProvider = ({ children }) => {
     const data = await response.json();
     dispatch({ type: 'GET_USERS', payload: data });
   };
+
+  // Set loading:
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
   return (
     <GithubContext.Provider
