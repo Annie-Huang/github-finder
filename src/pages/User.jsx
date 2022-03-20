7676267d688e88827dcfbedbd87cb2429dea7860
Annie Huang <annie.hn@gmail.com>
2022-03-20 21:41:13 +1100

    Create the "Back To Search" button in User page.

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -12,9 +12,36 @@ const User = () => {
 
   useEffect(() => getUser(params.login), []);
 
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
   if (loading) return <Spinner />;
 
-  return <div>{user.login}</div>;
+  return (
+    <>
+      <div className='w-full mx-auto lg:w-10/12'>
+        <div className='mb-4'>
+          <Link to='/' className='btn btn-ghost'>
+            Back To Search
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default User;
