24753dc0fb7c89a5fbe805c7d091c7ca52b7c237
Annie Huang <annie.hn@gmail.com>
2022-03-20 21:35:31 +1100

    Show spinner in the user page while it's loading the user info from the api.

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,15 +1,20 @@
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
 import { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import GithubContext from '../context/github/GithubContext';
+import Spinner from '../components/layout/Spinner';
 
 const User = () => {
-  const { getUser, user } = useContext(GithubContext);
+  const { getUser, user, loading } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => getUser(params.login), []);
 
-  return <div>User</div>;
+  if (loading) return <Spinner />;
+
+  return <div>{user.login}</div>;
 };
 
 export default User;
