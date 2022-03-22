9a98ddb97019290373e19f3f751c380e864304cf
Annie Huang <annie.hn@gmail.com>
2022-03-22 17:47:35 +1100

    Add name and login onto the avatar image.

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -46,6 +46,10 @@ const User = () => {
               <figure>
                 <img src={avatar_url} alt='' />
               </figure>
+              <div className='card-body justify-end'>
+                <h2 className='card-title mb-0'>{name}</h2>
+                <p>{login}</p>
+              </div>
             </div>
           </div>
         </div>
