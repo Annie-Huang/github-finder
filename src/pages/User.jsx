52bc4a66fa90628760a971b09dfc816c28f51d3c
Annie Huang <annie.hn@gmail.com>
2022-03-22 17:59:18 +1100

    Show name, badges and bio next to the image.

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -52,6 +52,19 @@ const User = () => {
               </div>
             </div>
           </div>
+
+          <div className='col-span-2'>
+            <div className='mb-6'>
+              <h1 className='text-3xl card-title'>
+                {name}
+                <div className='ml-2 mr-1 badge badge-success'>{type}</div>
+                {hireable && (
+                  <div className='mx-1 badge badge-info'>Hireable</div>
+                )}
+              </h1>
+              <p>{bio}</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
