7256160ef5a846cf921e3f7b8d4280a1837c7104
Annie Huang <annie.hn@gmail.com>
2022-03-26 16:05:06 +1100

    Add Follower and Following info

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -113,6 +113,27 @@ const User = () => {
             </div>
           </div>
         </div>
+        <div className='w-full py-5 mb-6 rounded-lg shadow-mg bg-base-100 stats'>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaUsers className='text-3xl md:text-5xl' />
+            </div>
+            <div className='stat-title pr-5'>Followers</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
+              {followers}
+            </div>
+          </div>
+
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaUserFriends className='text-3xl md:text-5xl' />
+            </div>
+            <div className='stat-title pr-5'>Following</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
+              {following}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
