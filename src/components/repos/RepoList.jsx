4580128814c348f3ef250a0668ed611faa08507a
Annie Huang <annie.hn@gmail.com>
2022-03-28 21:45:35 +1100

    Add RepoList content

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
 const RepoList = ({ repos }) => {
-  return <div>Repos</div>;
+  return (
+    <div className='lg:tab-rounded-lg shadow-lg car bg-base-100'>
+      <div className='card-body'>
+        <h2 className='text-3xl my-4 font-bold card-title'>Top Repositories</h2>
+        {repos.map((repo) => (
+          <h3>{repo.name}</h3>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+RepoList.propTypes = {
+  repos: PropTypes.array.isRequired,
 };
 
 export default RepoList;
