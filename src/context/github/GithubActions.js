51c8e88e97f187a62cc153585c6bd495abbc077b
Annie Huang <annie.hn@gmail.com>
2022-03-30 21:25:03 +1100

    Get rid of setLoading in getUser and getUserRepos and only return data instead of dispatch actions as well.

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -18,8 +18,8 @@ export const searchUsers = async (text) => {
 };
 
 // Get single user
-const getUser = async (login) => {
-  setLoading();
+export const getUser = async (login) => {
+  // setLoading();
 
   const response = await fetch(`${GITHUB_URL}/users/${login}`);
 
@@ -27,13 +27,14 @@ const getUser = async (login) => {
     window.location = '/notfound';
   } else {
     const data = await response.json();
-    dispatch({ type: 'GET_USER', payload: data });
+    // dispatch({ type: 'GET_USER', payload: data });
+    return data;
   }
 };
 
 // Get user repos
-const getUserRepos = async (login) => {
-  setLoading();
+export const getUserRepos = async (login) => {
+  // setLoading();
 
   const params = new URLSearchParams({
     sort: 'created',
@@ -45,5 +46,6 @@ const getUserRepos = async (login) => {
   const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
 
   const data = await response.json();
-  dispatch({ type: 'GET_REPOS', payload: data });
+  // dispatch({ type: 'GET_REPOS', payload: data });
+  return data;
 };
